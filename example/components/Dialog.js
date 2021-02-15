95d6f5c09389f2c5ddb78a61f22fcb22a56041f3
Timo Hillmann <timo@hillmann.cc>
2021-02-16 09:07:06 +1000

    feat: add dialog animation

--- example/components/Dialog.js
+++ example/components/Dialog.js
@@ -1,24 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DialogOverlay, DialogContent } from "@reach/dialog";
+import { Transition } from "@headlessui/react";
 import { CloseIcon } from "./CloseIcon";
 
-export function Dialog({ children, "aria-label": ariaLabel, ...props }) {
+export function Dialog({
+  children,
+  "aria-label": ariaLabel,
+  isOpen,
+  ...props
+}) {
+  const [mounted, setMounted] = useState(isOpen);
+  useEffect(() => {
+    if (isOpen) {
+      setMounted(true);
+    }
+  }, [isOpen]);
   return (
     <DialogOverlay
-      className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-25 flex justify-center items-center"
+      className="fixed top-0 left-0 w-full h-full"
+      isOpen={mounted}
       {...props}
     >
-      <div className="relative w-full max-w-3xl">
-        <button className="absolute -top-10 right-0 text-gray-700 focus:outline-none" onClick={props.onDismiss}>
-          <CloseIcon className="w-8 h-8" />
-        </button>
-        <DialogContent
-          aria-label={ariaLabel}
-          className="bg-white w-full shadow-lg rounded-lg p-6 outline-none"
-        >
-          {children}
-        </DialogContent>
-      </div>
+      <Transition
+        show={isOpen}
+        appear
+        static
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-25 flex justify-center items-center"
+        afterLeave={() => setMounted(false)}
+      >
+        <div className="relative w-full max-w-3xl">
+          <button
+            className="absolute -top-10 right-0 text-gray-700 focus:outline-none"
+            onClick={props.onDismiss}
+          >
+            <CloseIcon className="w-8 h-8" />
+          </button>
+          <DialogContent
+            aria-label={ariaLabel}
+            className="bg-white w-full shadow-lg rounded-lg p-6 outline-none"
+          >
+            {children}
+          </DialogContent>
+        </div>
+      </Transition>
     </DialogOverlay>
   );
 }
